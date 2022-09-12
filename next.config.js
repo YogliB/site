1be64db07926205a0cc2e049ec5cf365921e8f7b
YogliB <yogev.boaron-ben-har@skai.io>
2022-09-12 14:53:33 +0300

    add WindiCSS & Storybook

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
+const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: {
+    plugins: [new WindiCSSWebpackPlugin()]
+  }
 }
 
 module.exports = nextConfig
+
+
+
