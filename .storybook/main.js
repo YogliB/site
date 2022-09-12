210ec475a2d0479945f2790d5609b04bd2b38cf1
YogliB <yogev.boaron-ben-har@skai.io>
2022-09-12 14:53:42 +0300

    add WindiCSS & Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,21 @@
+const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
+
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  },
+  webpackFinal: (config) => {
+    config.plugins.push(new WindiCSSWebpackPlugin())
+    return config
+  },
+}
