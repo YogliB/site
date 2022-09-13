92bdde865b6956f7a6f96c0831efd6e05502f348
YogliB <yogev.boaron-ben-har@skai.io>
2022-09-13 12:01:33 +0300

    add preact

--- next.config.js
+++ next.config.js
@@ -1,16 +1,14 @@
-const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
+const withPreact = require('next-plugin-preact');
+const WindiCSSWebpackPlugin = require('windicss-webpack-plugin');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  webpack(config) {
-    config.plugins.push(new WindiCSSWebpackPlugin())
-    return config
-  },
-}
+const nextConfig = withPreact({
+	reactStrictMode: true,
+	swcMinify: true,
+	webpack(config) {
+		config.plugins.push(new WindiCSSWebpackPlugin())
+		return config
+	},
+});
 
-module.exports = nextConfig
-
-
-
+module.exports = nextConfig;
